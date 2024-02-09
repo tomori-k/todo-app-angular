a269e146872d6cf700f6ff9ee9b32da55b999318
tomori-k <108829509+tomori-k@users.noreply.github.com>
2024-02-09 17:51:19 +0900

    Router の設定

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { TodoPageComponent } from './pages/todo-page/todo-page.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'list',
+    component: TodoPageComponent,
+    title: 'TODO',
+  },
+]
---
+++ src/app/pages/todo-page/todo-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-page.component.html',
+  styleUrl: './todo-page.component.scss'
+})
+export class TodoPageComponent {
+
+}
