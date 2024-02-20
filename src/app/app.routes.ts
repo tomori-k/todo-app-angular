6270b215fc3382bbec0d1a7fbcf6c531fa0341c1
tomori-k <108829509+tomori-k@users.noreply.github.com>
2024-02-20 15:27:08 +0900

    編集ページと404ページの追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { TodoPageComponent } from './pages/todo-page/todo-page.component'
 import { TodoCreateComponent } from './pages/todo-create/todo-create.component'
+import { TodoEditComponent } from './pages/todo-edit/todo-edit.component'
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component'
 
 export const routes: Routes = [
   {
@@ -18,5 +20,14 @@ export const routes: Routes = [
     path: 'create',
     component: TodoCreateComponent,
     title: 'Add TODO',
+  },
+  {
+    path: 'edit/:id',
+    component: TodoEditComponent,
+    title: 'Edit TODO',
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
   },
 ]
---
+++ src/app/pages/todo-edit/todo-edit.component.ts
@@ -0,0 +1,93 @@
+import { Component, Input } from '@angular/core'
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
+import { Todo, TodoCategory } from '../../models'
+import { CommonModule } from '@angular/common'
+
+function getTodo(id: number): Todo {
+  // 一旦ダミーデータを返す
+  return {
+    id: id,
+    categoryId: 1,
+    title: 'Improve design',
+    body: 'improve the design of the header',
+    state: 0,
+    updatedAt: new Date(2000, 5, 14, 12, 31, 59),
+    createdAt: new Date(2000, 5, 14, 12, 31, 59),
+  }
+}
+
+@Component({
+  selector: 'app-todo-edit',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './todo-edit.component.html',
+  styleUrl: './todo-edit.component.scss',
+})
+export class TodoEditComponent {
+  public readonly formGroup = new FormGroup({
+    title: new FormControl('', Validators.required),
+    body: new FormControl('', Validators.required),
+    category: new FormControl(0),
+    state: new FormControl(0),
+  })
+
+  // ダミーデータ
+  public readonly categories: TodoCategory[] = [
+    {
+      id: 1,
+      name: 'frontend',
+      color: '1',
+      updatedAt: new Date(2021, 3, 9, 7, 48, 56),
+      createdAt: new Date(2021, 3, 9, 4, 8, 57),
+    },
+    {
+      id: 2,
+      name: 'backend',
+      color: '2',
+      updatedAt: new Date(2021, 3, 9, 7, 48, 56),
+      createdAt: new Date(2021, 3, 9, 4, 8, 57),
+    },
+    {
+      id: 3,
+      name: 'infra',
+      color: '3',
+      updatedAt: new Date(2021, 3, 9, 7, 48, 56),
+      createdAt: new Date(2021, 3, 9, 4, 8, 57),
+    },
+  ]
+
+  // path parameters
+
+  @Input()
+  set id(todoId: number) {
+    const todo = getTodo(todoId)
+
+    this.formTitle.setValue(todo.title)
+    this.formBody.setValue(todo.body)
+    this.formCategory.setValue(todo.categoryId)
+    this.formState.setValue(todo.state)
+  }
+
+  // form controls
+
+  get formTitle() {
+    return this.formGroup.controls.title
+  }
+
+  get formBody() {
+    return this.formGroup.controls.body
+  }
+
+  get formCategory() {
+    return this.formGroup.controls.category
+  }
+
+  get formState() {
+    return this.formGroup.controls.state
+  }
+}
