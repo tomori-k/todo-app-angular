f74e57034a725bdd7f7b4e4daa887f065ced5a61
tomori-k <108829509+tomori-k@users.noreply.github.com>
2024-02-27 16:44:05 +0900

    TodoCategoryServiceの実装

--- src/app/services/todo-category-service.ts
+++ src/app/services/todo-category-service.ts
@@ -5,31 +5,11 @@ import { TodoCategory } from '../models'
   providedIn: 'root',
 })
 export class TodoCategoryService {
-  private readonly categories: TodoCategory[] = [
-    {
-      id: 1,
-      name: 'frontend',
-      color: '1',
-      updatedAt: new Date(2021, 3, 9, 7, 48, 56),
-      createdAt: new Date(2021, 3, 9, 4, 8, 57),
-    },
-    {
-      id: 2,
-      name: 'backend',
-      color: '2',
-      updatedAt: new Date(2021, 3, 9, 7, 48, 56),
-      createdAt: new Date(2021, 3, 9, 4, 8, 57),
-    },
-    {
-      id: 3,
-      name: 'infra',
-      color: '3',
-      updatedAt: new Date(2021, 3, 9, 7, 48, 56),
-      createdAt: new Date(2021, 3, 9, 4, 8, 57),
-    },
-  ]
-
   public async getAll(): Promise<TodoCategory[]> {
-    return this.categories
+    const response = await fetch('http://localhost:9000/todo-category/list', {
+      method: 'GET',
+    })
+    const todoCategoryList = await response.json()
+    return todoCategoryList
   }
 }
